ed62e078b818fa5b22b5d159b0c57754f1832060
Dylberto <dylang565@gmail.com>
2020-04-21 15:49:03 +0100

    Lights spawn

---
+++ Lab_Test_2/Assets/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public int number_of_lights = 10;
+
+    public float radius = 10f;
+
+
+    void Start()
+    {
+        for (int i = 0; i < number_of_lights; i++)
+        {
+            float angle = i * Mathf.PI * 2 / number_of_lights;
+
+            float x = Mathf.Cos(angle) * radius;
+            float z = Mathf.Sin(angle) * radius;
+
+            Vector3 pos = transform.position + new Vector3(x, 0, z);
+
+            float angle_degrees = -angle * Mathf.Rad2Deg;
+
+            Quaternion rotation = Quaternion.Euler(0, angle_degrees, 0);
+
+
+            Instantiate(prefab, pos, rotation);
+        }
+    }
+
+}
