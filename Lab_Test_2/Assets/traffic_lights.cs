f0060cec783bf3ec1b4fb537753d8dc50c80714f
Dylberto <dylang565@gmail.com>
2020-04-21 16:12:58 +0100

    Random start color set

---
+++ Lab_Test_2/Assets/traffic_lights.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class traffic_lights : MonoBehaviour
+{
+    private Renderer ren;
+
+    private int start_color;
+
+
+    void Start()
+    {
+        ren = gameObject.GetComponent<Renderer>();
+
+        start_color = Random.Range(1 ,4);
+    }
+
+    void Update()
+    {
+        if(start_color == 1f)
+        {
+            ren.material.color = new Color(0, 255, 0);
+        }
+
+        if(start_color == 2f)
+        {
+            ren.material.color = new Color(255, 255, 0);
+        }
+
+        if (start_color == 3f)
+        {
+            ren.material.color = new Color(255, 0, 0);
+        }
+
+    }
+
+}
